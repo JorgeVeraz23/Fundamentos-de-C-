1b9b2a03c00bbf0e731bf5b8dbf9aa355651e6be
JorgeVeraz23 <JorgeVeraZambrano@outlook.com>
2023-07-24 18:29:51 -0500

    first commit

---
+++ Clases Ojetos y Constructores/Models/Cerveza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosC_.Models
+{
+    internal class Cerveza : Bebida
+    {
+        public Cerveza() : base("Cerveza",500)//base hace referencia al constructor de su padre
+        {
+
+        }
+    }
+}
---
+++ Clases Ojetos y Constructores/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FundamentosC_.Models;
+
+
+namespace FundamentosCSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Bebida bebida = new Bebida("Coca cola", 1000);
+            bebida.Beberse(500);
+            Console.WriteLine(bebida.Cantidad);*/
+            Cerveza cerveza = new Cerveza();
+            cerveza.Beberse(10);
+        }
+    }
+}
