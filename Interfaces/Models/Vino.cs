fb21ea0bc759792524298fb45ea10ede07062a66
JorgeVeraz23 <JorgeVeraZambrano@outlook.com>
2023-07-24 21:17:22 -0500

    first commit

---
+++ Interfaces/Models/Vino.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosC_.Models
+{
+    internal class Vino : Bebida, IBebidaAlcoholica
+    {
+        public int Alcohol { get; set; }
+        public void MaxRecomendado()
+        {
+            Console.WriteLine("El maximo permitido son 3 copas");
+        }
+        public Vino(string Nombre, int Cantidad) : base(Nombre, Cantidad)//base hace referencia al constructor de su padre
+        {
+
+        }
+
+
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FundamentosC_.Models;
+
+
+namespace FundamentosCSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bebidaAlcoholica = new Vino("Gato Negro", 100);
+            MostrarRecomendacion(bebidaAlcoholica);
+        }
+        static void MostrarRecomendacion(IBebidaAlcoholica bebida)
+        {
+            bebida.MaxRecomendado();
+        }
+    }
+}
+
